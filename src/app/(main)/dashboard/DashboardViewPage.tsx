13a3eba9010d4e1672c5fe068f288a05544822cd
maotk <yiboteaching@gmail.com>
2026-08-19 11:37:52 +0800

    Update DashboardViewPage.tsx

--- src/app/(main)/dashboard/DashboardViewPage.tsx
+++ src/app/(main)/dashboard/DashboardViewPage.tsx
@@ -1,46 +1,4 @@
 'use client';
-import { Column } from '@umami/react-zen';
-import { useEffect } from 'react';
-import { BoardControls } from '@/app/(main)/boards/[boardId]/BoardControls';
-import { BoardViewBody } from '@/app/(main)/boards/[boardId]/BoardViewBody';
-import { Empty } from '@/components/common/Empty';
-import { PageBody } from '@/components/common/PageBody';
-import { useBoard, useMessages, useNavigation } from '@/components/hooks';
-import { DashboardProvider } from './DashboardProvider';
-import { DashboardViewHeader } from './DashboardViewHeader';
-
-function DashboardContent() {
-  const { board } = useBoard();
-  const { t, messages } = useMessages();
-  const rows = board?.parameters?.rows ?? [];
-  const hasComponents = rows.some(row => row.columns?.some(column => !!column.component));
-
-  if (!hasComponents) {
-    return <Empty message={t(messages.emptyDashboard)} />;
-  }
-
-  return <BoardViewBody />;
-}
-
-export function DashboardViewPage() {
-  const { teamId, router } = useNavigation();
-
-  useEffect(() => {
-    if (teamId) {
-      router.replace('/dashboard');
-    }
-  }, [teamId, router]);
-
-  if (teamId) {
-    return null;
-  }
-
-  return (
-    <DashboardProvider>
-      <PageBody>
-        <Column>
-          <DashboardViewHeader />
-          'use client';
 
 import { Column, Text } from '@umami/react-zen';
 import { useEffect } from 'react';
@@ -87,7 +45,6 @@ export function DashboardViewPage() {
     <DashboardProvider>
       <PageBody>
         <Column gap="6">
-
           <DashboardViewHeader />
 
           {/* 全部网站总览 */}
@@ -102,14 +59,6 @@ export function DashboardViewPage() {
           {/* Umami 原有仪表盘功能 */}
           <BoardControls />
 
-          <DashboardContent />
-
-        </Column>
-      </PageBody>
-    </DashboardProvider>
-  );
-}
-          <BoardControls />
           <DashboardContent />
         </Column>
       </PageBody>
